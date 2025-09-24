184b54f115a8887e78ad971c280fe8540cc4e6fb
mahadi609im <maha609im@gmail.com>
2025-09-24 08:59:25 +0600

    modal show

--- src/Components/SelectPlayers/SelectPlayer.jsx
+++ src/Components/SelectPlayers/SelectPlayer.jsx
@@ -1,6 +1,18 @@
 import { RiDeleteBin2Fill } from 'react-icons/ri';
+import { FaFlag, FaUser } from 'react-icons/fa';
+
 const SelectPlayer = ({ choosePl, handlePlayerRemove }) => {
-  const { id, player_name, playing_role, player_image, price } = choosePl;
+  const {
+    id,
+    player_name,
+    playing_role,
+    player_image,
+    price,
+    player_country,
+    rating,
+    bating_style,
+    bowling_style,
+  } = choosePl;
   return (
     <div className="p-4 md:p-6 border border-[#ffffff1a] bg-[#1f2730] w-full rounded-2xl flex justify-between items-center">
       <div className="flex gap-3 md:gap-6 items-center">
@@ -21,11 +33,67 @@ const SelectPlayer = ({ choosePl, handlePlayerRemove }) => {
         </div>
       </div>
       <div
-        onClick={() => handlePlayerRemove(id)}
+        onClick={() => document.getElementById('my_modal_3').showModal()}
         className="cursor-pointer w-[50px] h-[50px] flex items-center justify-center border-2 border-[#ffffff1a] rounded-lg hover:bg-[#ffffff1a] "
       >
         <RiDeleteBin2Fill className="text-[#F14749] text-3xl" />
       </div>
+      <dialog id="my_modal_3" className="modal">
+        <div className="modal-box">
+          <form method="dialog">
+            <button className="cursor-pointer w-[30px] h-[30px] flex items-center justify-center border-2 border-[#ffffff1a] rounded-lg hover:bg-[#ffffff1a] absolute right-0 top-0">
+              ✕
+            </button>
+          </form>
+          <div className="p-6 border border-[#ffffff1a] rounded-2xl shadow-lg bg-[#1f2730]">
+            <div className="w-1/2 h-auto border border-[#ffffff1a]  rounded-2xl mb-6 mx-auto">
+              <img
+                className="object-cover w-full h-full rounded-2xl"
+                src={player_image}
+                alt=""
+              />
+            </div>
+            <div>
+              <div className="space-y-4">
+                <div className="flex items-center justify-between">
+                  <div className="flex gap-4 items-center">
+                    <FaUser className="text-2xl" />
+                    <h3 className="text-xl font-semibold">{player_name}</h3>
+                  </div>
+                  <p className="text-base font-normal text-[#fffcffb3]">
+                    {rating}
+                  </p>
+                </div>
+                <div className="flex gap-4 items-center justify-between">
+                  <div className="flex gap-3 items-center">
+                    <FaFlag className="text-xl text-[#fffcffb3]" />
+                    <p className="text-base font-normal text-[#fffcffb3]">
+                      {player_country}
+                    </p>
+                  </div>
+                  <p className="py-2 px-5 text-[#fffcffb3] rounded-lg text-sm font-medium border border-[#ffffff1a]">
+                    {playing_role} |{' '}
+                    {playing_role == 'Bowler' ? bowling_style : bating_style}
+                  </p>
+                </div>
+                <div className="border-b border-b-[#fffcff3b]"></div>
+                <div className="flex justify-between items-center">
+                  <h5 className="text-base font-medium">
+                    Price: <span className="text-[#E7FE29]">{price}</span>{' '}
+                    <span className="text-[#E7FE29]">USD</span>
+                  </h5>
+                  <button
+                    onClick={() => handlePlayerRemove(id)}
+                    className="cursor-pointer w-[50px] h-[50px] flex items-center justify-center border-2 border-[#ffffff1a] rounded-lg hover:bg-[#ffffff1a] "
+                  >
+                    <RiDeleteBin2Fill className="text-[#F14749] text-3xl" />
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </dialog>
     </div>
   );
 };
